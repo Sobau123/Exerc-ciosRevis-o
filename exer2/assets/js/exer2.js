849034b6b803c1a1df0160943b521b235af08d84
Jonatan <jonatangoncalves598@gmail.com>
2023-11-09 14:21:18 -0300

    correcoes

--- exer2/assets/js/exer2.js
+++ exer2/assets/js/exer2.js
@@ -1,25 +1,25 @@
-let sumAgeOtimo = 0;
+let mediaIdadeOtimo = 0;
 let countOtimo = 0;
-let countRegular = 0;
+let qtdeRegular = 0;
 let countTotal = 0;
 let countBom = 0;
 
 function submitSurvey() {
-  const age = parseInt(document.getElementById("age").value);
+  const idade = parseInt(document.getElementById("idade").value);
   const opinion = parseInt(document.getElementById("opinion").value);
 
   if (opinion === 3) {
-    sumAgeOtimo += age;
+    mediaIdadeOtimo += idade;
     countOtimo++;
   } else if (opinion === 1) {
-    countRegular++;
+    qtdeRegular++;
   } else if (opinion === 2) {
     countBom++;
   }
 
   countTotal++;
 
-  document.getElementById("avgAgeOtimo").textContent = countOtimo > 0 ? (sumAgeOtimo / countOtimo).toFixed(2) : '-';
-  document.getElementById("countRegular").textContent = countRegular;
+  document.getElementById("mediaIdadeOtimo").textContent = countOtimo > 0 ? (mediaIdadeOtimo / countOtimo).toFixed(2) : '-';
+  document.getElementById("qtdeRegular").textContent = qtdeRegular;
   document.getElementById("percentageBom").textContent = ((countBom / countTotal) * 100).toFixed(2) + "%";
 }
